fd84b7879c746dcb8421bee21b72ea3eca64aa86
sanSkiller <1290148953@qq.com>
2020-10-21 09:07:10 +0800

    updated:第一次互动

--- demos/Webscoket实现简易聊天室/server/index.js
+++ demos/Webscoket实现简易聊天室/server/index.js
@@ -4,4 +4,8 @@ const wss = new WebSocket.Server({ port: 8888 });
 
 wss.on("connection", (ws) => {
   console.log("one client is connection");
+
+  ws.on("message", (msg) => {
+    ws.send("hello client");
+  });
 });
