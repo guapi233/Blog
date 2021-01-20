e297fa03e18aea0ec9c14d97e7c7d33b0614847e
sanSkiller <1290148953@qq.com>
2021-01-20 20:40:58 +0800

    updated:【628】三个数的最大乘积

---
+++ algorithms/【628】三个数的最大乘积/排序.js
@@ -0,0 +1,8 @@
+var maximumProduct = function (nums) {
+  nums.sort((a, b) => a - b);
+  const n = nums.length;
+  return Math.max(
+    nums[0] * nums[1] * nums[n - 1],
+    nums[n - 1] * nums[n - 2] * nums[n - 3]
+  );
+};
